bc61c2f3a0fada66d7dc273da14f37075fa863a9
yumanyer <manuelpisoni09@gmail,com>
2024-02-03 17:25:32 -0300

    barra de navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+// exportacion por defecto
+// import pochcolo from "./components/layout/Navbar"
+import {Navbar} from "./components/layout/Navbar"
 
 function App() {
 
 
   return 
-
+  <Navbar />
   
 }
 
---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,14 @@
+// exportacion nombrada
+// export const pochoclo = ()=>{
+
+// }
+
+export const productos =[]
+
+export const Navbar = ()=>{
+    return(
+        <h1>Este es elñ Navbar</h1>
+    )
+}
+
+// export default pochcolo (exportacion por defecto )
