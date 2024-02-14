58cff29d0eb2d98f3a6448f8ed2ba88f699113c8
yumanyer <manuelpisoni09@gmail,com>
2024-02-14 15:03:36 -0300

    clonacion a la rama 6

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,32 +1,27 @@
-// exportacion nombrada
-// export const pochoclo = ()=>{
+import React from "react";
+import CartWidget from "../common/CartWidget";
+import { Conter } from "../common/Conter"; // Importar Conter como una exportación nombrada
 
-import CartWidget from "../common/CartWidget"
-import { Conter } from "../common/Conter"
+export const pochoclo = () => {
+  // Exportación nombrada
+};
 
-// }
-
-
-
-export const Navbar = ()=>{
-    return(
-        <nav>
-            <div>
-                <ul>
-                    <li>Home</li>
-                    <li>hambrugesa nuevas</li>
-                    <li>Las mas pedidas</li>
-                    <li>Carta</li>
-                </ul>
-            <div>
-                <img src="" alt="logo" />
-            </div>
-            
-            <CartWidget />
-            <Conter />
-            </div>
-        </nav>
-    )
-}
-
-// export default pochcolo (exportacion por defecto )
+export const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>hambrugesa nuevas</li>
+          <li>Las mas pedidas</li>
+          <li>Carta</li>
+        </ul> {/* Cierre de la etiqueta ul movido aquí */}
+        <div>
+          <img src="" alt="" />
+        </div>
+        <CartWidget />
+        <Conter /> {/* Uso de Conter como componente importado */}
+      </div>
+    </nav>
+  );
+};
