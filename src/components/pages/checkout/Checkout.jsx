b67577b2cf039238c99526dbd396da45da406193
yumanyer <manuelpisoni09@gmail,com>
2024-03-22 14:15:08 -0300

    falta 100% de logica al stock

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 
-const Checkout = ({ envioDeFormulario, capturarData }) => {
+const Checkout = ({ envioDeFormulario, capturarData, orderId }) => {
   return (
     <>
-      <h1>Aca va el checkout</h1>
-{/* el nombre del input = la propiedad que quiero modificar */}
-      <form onSubmit={envioDeFormulario}>
-        <input type="text" placeholder="ingresa tu nombre"  onChange={capturarData} name="name"/>
-        <input type="text" placeholder="ingresa tu apellido"onChange={capturarData} name="lastName"/>
-        <button>Enviar</button>
-      </form>
+      {/* el nombre del input = la propiedad que quiero modificar */}
+      {orderId ? (
+        <h1>
+          Gracias por su compra <br />su numero de compra es {orderId}
+        </h1>
+      ) : (
+        <form onSubmit={envioDeFormulario}>
+          <input
+            type="text"
+            placeholder="ingresa tu nombre"
+            onChange={capturarData}
+            name="name"
+          />
+          <input
+            type="number"
+            placeholder="ingresa tu telefono"
+            onChange={capturarData}
+            name="phone"
+          />
+          <input
+            type="text"
+            placeholder="ingresa tu email"
+            onChange={capturarData}
+            name="email"
+          />
+          <button>Comprar</button>
+        </form>
+      )}
     </>
   );
 };
--- src/components/pages/checkout/CheckoutContainer.jsx
+++ src/components/pages/checkout/CheckoutContainer.jsx
@@ -1,34 +1,54 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Checkout from "./Checkout";
+import { CartContext } from "../../../context/CartContext";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../../firebaseConfig";
+
 
 const CheckoutContainer = () => {
   // creamos un estado para capturar informacion del usario
   const [userInfo, setUserInfo] = useState({
     name: "",
-    lastName: "",
+    phone: "",
+    email: "",
   });
+
+  // Aquí, setOrderId debería ser una función para actualizar el orderId, no una variable constante
+  const [orderId, setOrderId] = useState(null);
+
+  const { cart, precioTotal } = useContext(CartContext);
+  let totalPrice = precioTotal();
   //   la funcion se dispara cuando envio el form (LA VALIDACION DE DATOS TIENE QUE SER EN EL ENVIO DE FORM)
   const envioDeFormulario = (event) => {
-    event.preventDefault();
     //no se recarga la pag si se envia el form
     // manipulacion de datos
-    console.log("envio de formulario");
-    console.log(userInfo);
-  };
+    event.preventDefault();
+    // FUNCION
+    let order = {
+      //info cliente
+      buyer: userInfo,
+      //info items
+      items: cart,
+      //info precio total
+      total: totalPrice,
+    };
 
+    let ordersCollection = collection(db, "orders");
+    addDoc(ordersCollection, order).then((resp) => setOrderId(resp.id)); // Corregido aquí
+  };
 
   const capturarData = (event) => {
     // seteo userinfo con un objet
-    // para no perder datos user el ...userInfo y cuando encuentra otra prop 
+    // para no perder datos user el ...userInfo y cuando encuentra otra prop
     // que ya tenia deja la que tenia y toma la que se encontro
-    setUserInfo({...userInfo, [event.target.name]:event.target.value});
-  }
+    setUserInfo({ ...userInfo, [event.target.name]: event.target.value });
+  };
   return (
     <Checkout
+      orderId={orderId}
       envioDeFormulario={envioDeFormulario}
       capturarData={capturarData}
     />
   );
 };
-
 export default CheckoutContainer;
